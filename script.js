92296b35d62c8da8cf556005725dec7ab6682b79
Pandorumn <70848056+Pandorumn@users.noreply.github.com>
2022-05-30 22:14:46 +0300

    "Work First" and "Study First"

--- script.js
+++ script.js
@@ -65,6 +65,7 @@ const rest = "&#9775;"
 const separator = new Event("", 0, 0)
 
 let eventsTemplates = [
+  // Work First
   [
     new Event("Утренний туалет", 0, 25, 6, 15),
     new Event("Новости", 0, 15),
@@ -93,9 +94,39 @@ let eventsTemplates = [
     new Event("Подготовка ко сну", 0, 25),
     new Event("Сон", 8, 30),
   ],
+  // Study First
+  [
+    new Event("Утренний туалет", 0, 25, 6, 15),
+    new Event("Новости", 0, 15),
+    new Event("Зарядка", 0, 10),
+    new Event("Речь", 0, 10),
+    new Event("Растяжка", 0, 10),
+    new Event("Завтрак", 0, 20),
+    new Event("~", 0, 15),
+    separator,
+    new Event("~", 1, 30),
+    new Event("Ланч", 0, 15),
+    separator,
+    new Event(work, 2, 00),
+    new Event("Обед", 0, 15),
+    new Event(work, 2, 00),
+    new Event("Nap", 0, 30),
+    new Event("Полдник", 0, 15),
+    new Event(work, 2, 00),
+    new Event("Ужин", 0, 15),
+    new Event(work, 2, 00),
+    separator,
+    new Event("Речь", 0, 10),
+    new Event("Растяжка", 0, 10),
+    new Event("Душ", 0, 15),
+    new Event("Новости", 0, 45),
+    new Event("~", 1, 00),
+    new Event("Подготовка ко сну", 0, 25),
+    new Event("Сон", 8, 30),
+  ],
 ]
 
-let indexDescr = ["Recovery"]
+let indexDescr = ["Work First", "Study First"]
 let currentIndex = localStorage.getItem("currentIndex") || 0
 if (currentIndex >= eventsTemplates.length) currentIndex = 0
 let events = eventsTemplates[currentIndex]
