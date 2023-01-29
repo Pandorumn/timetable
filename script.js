a6fc369b85d7828f897cffbd0aac7b1736f6c717
Pandorumn <70848056+Pandorumn@users.noreply.github.com>
2023-01-29 20:30:53 +0200

    get up at 8:15

--- script.js
+++ script.js
@@ -66,7 +66,7 @@ const separator = new Event("", 0, 0)
 
 let eventsTemplates = [
   [
-    new Event("Утренний туалет", 0, 25, 8, 00),
+    new Event("Утренний туалет", 0, 25, 8, 15),
     new Event("Новости", 0, 15),
     new Event("Речь", 0, 5),
     new Event("Зарядка", 0, 15),
