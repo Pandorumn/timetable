d2b672603ba6fcd9ab94e9b88da0af69684c45b7
Pandorumn <70848056+Pandorumn@users.noreply.github.com>
2023-04-25 18:51:19 +0300

    get up earlier to do ~ before work

--- script.js
+++ script.js
@@ -99,7 +99,35 @@ eventsTemplates.push([
   new Event("Сон", 10, 00),
 ])
 
-let indexDescr = ["", "More sleep"]
+eventsTemplates.push([
+  new Event("Утренний туалет", 0, 30, 6, 15),
+  new Event("Новости", 0, 15),
+  new Event("Зарядка", 0, 15),
+  separator,
+  new Event("~", 2, 00),
+  separator,
+  new Event("Тренировка", 0, 15),
+  new Event("Завтрак", 0, 30),
+  separator,
+  new Event(work, 2, 00),
+  new Event("Тренировка", 0, 15),
+  new Event("Ланч", 0, 15),
+  new Event(work, 2, 00),
+  new Event("Тренировка", 0, 15),
+  new Event("Обед", 0, 15),
+  new Event(work, 2, 00),
+  separator,
+  new Event("Тренировка", 0, 15),
+  new Event("Ужин", 0, 15),
+  new Event("Глаза", 0, 15),
+  new Event("Растяжка", 0, 45),
+  separator,
+  new Event("~", 2, 45),
+  separator,
+  new Event("Сон", 9, 00),
+])
+
+let indexDescr = ["", "More sleep", "Work in the middle"]
 let currentIndex = localStorage.getItem("currentIndex") || 0
 if (currentIndex >= eventsTemplates.length) currentIndex = 0
 let events = eventsTemplates[currentIndex]
